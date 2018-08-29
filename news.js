65f899473e6429e99d5049cdd4d28603b10d45ec
kirbisity <zhuy11@rpi.edu>
2018-08-28 22:44:55 -0400

    news section, dynamic create element

---
+++ news.js
@@ -0,0 +1,22 @@
+/* News can be added with title and description in the following format
+
+    , {
+        "Title": "",
+        "Description": ""
+    }
+
+ */
+
+
+let newsdata = [
+    {
+		"Title": "Sample news 1",
+		"Description": "I'm an Assistant Professor in the Department of Mechanical, Aerospace, and Nuclear Engineering (MANE) at Rensselaer Polytechnic Institute (RPI) working on novel ideas related to the generic area of intelligent aerospace structural systems."
+	}, {
+		"Title": "Sample news 2",
+		"Description": "I'm an Assistant Professor in the Department of Mechanical, Aerospace, and Nuclear Engineering (MANE) at Rensselaer Polytechnic Institute (RPI) working on novel ideas related to the generic area of intelligent aerospace structural systems."
+	}, {
+        "Title": "Sample news 3",
+        "Description": "I'm an Assistant Professor in the Department of Mechanical, Aerospace, and Nuclear Engineering (MANE) at Rensselaer Polytechnic Institute (RPI) working on novel ideas related to the generic area of intelligent aerospace structural systems."
+    }
+]
